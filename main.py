106ecab95ed025f46cd206b16804a837046404ad
niccololaurora <niccolo.laurora@gmail.com>
2023-12-11 12:12:27 +0100

    main fixed

--- main.py
+++ main.py
@@ -18,11 +18,16 @@ def main():
 
     with open("file.txt", "a") as file:
         print(f"Parametri finali {params[0:20]}", file=file)
+        print("=" * 60, file=file)
 
     accuracy = my_class.test_loop()
 
     with open("file.txt", "a") as file:
+        print("/" * 60, file=file)
+        print("/" * 60, file=file)
         print(f"Accuracy {accuracy}", file=file)
+        print("/" * 60, file=file)
+        print("/" * 60, file=file)
 
 
 if __name__ == "__main__":
